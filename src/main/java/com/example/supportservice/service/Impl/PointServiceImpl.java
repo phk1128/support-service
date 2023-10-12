823aa8663955d127e554e2c1210dc49de9d820b1
박형균 <phk@baghyeong-gyun-ui-MacBookPro.local>
2023-10-12 18:54:07 +0900

    feat: Import localdatetime

--- src/main/java/com/example/supportservice/service/Impl/PointServiceImpl.java
+++ src/main/java/com/example/supportservice/service/Impl/PointServiceImpl.java
@@ -16,6 +16,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 
 
 @Service
