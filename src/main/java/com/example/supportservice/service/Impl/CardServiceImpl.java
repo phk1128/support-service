9ec857b31a1c543c7e1f204e5d86c49956cbc0c5
박형균 <phk@baghyeong-gyun-ui-MacBookPro.local>
2023-10-13 23:03:08 +0900

    fix: Fix UnexpectedRollbackException

--- src/main/java/com/example/supportservice/service/Impl/CardServiceImpl.java
+++ src/main/java/com/example/supportservice/service/Impl/CardServiceImpl.java
@@ -8,8 +8,10 @@ import com.example.supportservice.exception.AppException;
 import com.example.supportservice.exception.ErrorCode;
 import com.example.supportservice.service.CardService;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.apache.bcel.classfile.Module;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 
@@ -28,7 +30,7 @@ public class CardServiceImpl implements CardService {
         if (!cardRepository.existsByMember(member)) {
 
             Card newCard = Card.builder()
-                    .cardCompany(CardCompany.valueOf(cardCompany))
+                    .cardCompany(CardCompany.fromString(cardCompany))
                     .cardNumber(encoder.encode(cardNumber))
                     .member(member)
                     .build();
@@ -39,7 +41,7 @@ public class CardServiceImpl implements CardService {
     }
 
     @Override
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public String cardValidator(Member member) throws AppException{
 
         //서비스단에서 카드 정보 확인
