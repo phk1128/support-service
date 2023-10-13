cda4be67a0e7c5a4259349cd6c68e2a2df26d9cf
박형균 <phk@baghyeong-gyun-ui-MacBookPro.local>
2023-10-13 16:08:04 +0900

    feat: Add builder and updater

--- src/main/java/com/example/supportservice/domain/organization/model/Organization.java
+++ src/main/java/com/example/supportservice/domain/organization/model/Organization.java
@@ -1,6 +1,7 @@
 package com.example.supportservice.domain.organization.model;
 
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -21,11 +22,31 @@ public class Organization {
 
     private String contact;
 
+    private String url;
+
     @Enumerated(EnumType.STRING)
     private OrganizationStatus status;
 
     @Embedded
     private Address address;
+
+    @Builder
+    public Organization(String name, String contact, String url, OrganizationStatus status, Address address) {
+        this.name = name;
+        this.contact = contact;
+        this.url = url;
+        this.status = status;
+        this.address = address;
+
+    }
+
+    public void update(String name, String contact, String url, OrganizationStatus status, Address address) {
+        this.name = name;
+        this.contact = contact;
+        this.url = url;
+        this.status = status;
+        this.address = address;
+    }
 
 
 }
