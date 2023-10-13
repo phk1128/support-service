f1e071a6890a5166b884a264dfe535020cdbc22e
박형균 <phk@baghyeong-gyun-ui-MacBookPro.local>
2023-10-13 22:58:17 +0900

    feat: Add exitby method

--- src/main/java/com/example/supportservice/domain/subscribe/repository/SubscribeRepository.java
+++ src/main/java/com/example/supportservice/domain/subscribe/repository/SubscribeRepository.java
@@ -1,8 +1,11 @@
 package com.example.supportservice.domain.subscribe.repository;
 
+import com.example.supportservice.domain.organization.model.Organization;
 import com.example.supportservice.domain.subscribe.model.Subscribe;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SubscribeRepository extends JpaRepository<Subscribe, Long> {
+
+    boolean existsByOrganization(Organization organization);
 
 }
