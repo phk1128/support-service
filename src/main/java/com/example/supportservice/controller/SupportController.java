53764b00830b0d7308f25130b23e842cf4756ec4
박형균 <phk@baghyeong-gyun-ui-MacBookPro.local>
2023-10-13 10:57:25 +0900

    feat: Add donation api

--- src/main/java/com/example/supportservice/controller/SupportController.java
+++ src/main/java/com/example/supportservice/controller/SupportController.java
@@ -1,4 +1,25 @@
 package com.example.supportservice.controller;
 
+
+import com.example.supportservice.dto.SupportDto;
+import com.example.supportservice.service.SupportService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/v1/support")
+@RequiredArgsConstructor
 public class SupportController {
+
+    private final SupportService supportService;
+
+    @PostMapping("/donation")
+    public ResponseEntity<SupportDto.Response> donation(SupportDto.Save dto) {
+        return ResponseEntity.ok(supportService.save(dto));
+
+    }
+
 }
