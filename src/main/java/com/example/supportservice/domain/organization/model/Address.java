ca438b1cc5cbc1d575791112028564f9dfea9301
박형균 <phk@baghyeong-gyun-ui-MacBookPro.local>
2023-10-13 16:06:48 +0900

    docs: Modify docs

--- src/main/java/com/example/supportservice/domain/organization/model/Address.java
+++ src/main/java/com/example/supportservice/domain/organization/model/Address.java
@@ -13,10 +13,13 @@ public class Address {
     private String zipcode;
 
 
+    /*기본 생성자의 접근제한자를 protected로 설정하여 city, street, zipcode가 비어있는 Address 객체가 생성 되는 것을 방지
+    즉, Address 객체는 반드시 매개변수를 받게끔 하는것이 목적*/
     protected Address() {
 
     }
 
+    // 매개변수를 받는 생성자의 경우 접근제한자가 public이므로 상속없이 Address 객체를 생성가능
     public Address(String city, String street, String zipcode) {
 
         this.city = city;
